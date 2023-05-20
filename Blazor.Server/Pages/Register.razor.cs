3c7135fe520ec5989474fdbc7996814c398ffdb5
Patrick <patrick.rainer2@gmail.com>
2023-05-20 09:50:18 +0200

    Added Register Page

---
+++ Blazor.Server/Pages/Register.razor.cs
@@ -0,0 +1,19 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+
+namespace Blazor.Server.Pages;
+
+public partial class Register
+{
+    UserForCreationDto model = new UserForCreationDto();
+    bool success;
+
+    private void OnValidSubmit(EditContext context)
+    {
+        success = true;
+        StateHasChanged();
+        
+    }
+
+}
